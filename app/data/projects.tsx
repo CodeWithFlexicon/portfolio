65f0015c55b3553e7576110fa841e9ceb20cf66f
CodeWithFlexicon <Felixx.Chenn@gmail.com>
2026-04-09 10:56:36 -0400

    Update projects

--- app/data/projects.tsx
+++ app/data/projects.tsx
@@ -8,6 +8,12 @@ export type Project = {
 
 export const projects : Project[] = [
     {
+        name: "Card-catalyst",
+        description: "Shuffle the deck and click on a card to go some place random!",
+        github: "https://github.com/OracleFlow/CardCatalyst",
+        image: "/images/cardcatalyst.png",
+    },
+    {
         name: "Car-Catalog",
         description: "Catalog of cars from 2015-2020 with makes,trims and more!",
         github: "https://github.com/CodeWithFlexicon/Car-Catalog",
@@ -19,4 +25,10 @@ export const projects : Project[] = [
         github: "https://github.com/CodeWithFlexicon/chitchat-capstone",
         image: "/images/chitchat.png",
     },
+    {
+        name: "Dark Legions",
+        description: "Competitive deck builder",
+        github: "https://github.com/DarkLegionsCSGroup/dark-legions",
+        image: "/images/darklegions.png"
+    }
 ]
